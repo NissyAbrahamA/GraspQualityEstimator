07976abdaa06bc16f583e6cffffa2ed575492ca0
Nissy Abraham Abraham (Student) <220269470@csml01>
2023-09-25 14:10:32 +0100

    check

---
+++ convonets/train.py
@@ -0,0 +1,249 @@
+import torch
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+import numpy as np
+import os
+import argparse
+import time, datetime
+import matplotlib; matplotlib.use('Agg')
+from convonets.src import config, data
+from convonets.src.checkpoints import CheckpointIO
+from collections import defaultdict
+import shutil
+hasWandB = True
+try:
+    import wandb
+except ImportError:
+    hasWandB = False
+
+
+# Arguments
+parser = argparse.ArgumentParser(
+    description='Train a 3D reconstruction model.'
+)
+parser.add_argument('config', type=str, help='Path to config file.')
+parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
+parser.add_argument('--exit-after', type=int, default=-1,
+                    help='Checkpoint and exit after specified number of seconds'
+                         'with exit code 2.')
+
+args = parser.parse_args()
+cfg = config.load_config(args.config, 'configs/default.yaml')
+is_cuda = (torch.cuda.is_available() and not args.no_cuda)
+device = torch.device("cuda" if is_cuda else "cpu")
+# Set t0
+t0 = time.time()
+
+# Shorthands
+out_dir = cfg['training']['out_dir']
+batch_size = cfg['training']['batch_size']
+backup_every = cfg['training']['backup_every']
+vis_n_outputs = cfg['generation']['vis_n_outputs']
+exit_after = args.exit_after
+
+model_selection_metric = cfg['training']['model_selection_metric']
+if cfg['training']['model_selection_mode'] == 'maximize':
+    model_selection_sign = 1
+elif cfg['training']['model_selection_mode'] == 'minimize':
+    model_selection_sign = -1
+else:
+    raise ValueError('model_selection_mode must be '
+                     'either maximize or minimize.')
+
+# Output directory
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+shutil.copyfile(args.config, os.path.join(out_dir, 'config.yaml'))
+
+# Dataset
+train_dataset = config.get_dataset('train', cfg)
+val_dataset = config.get_dataset('val', cfg, return_idx=True)
+
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, num_workers=cfg['training']['n_workers'], shuffle=True,
+    collate_fn=data.collate_remove_none,
+    worker_init_fn=data.worker_init_fn)
+
+val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=1, num_workers=cfg['training']['n_workers_val'], shuffle=False,
+    collate_fn=data.collate_remove_none,
+    worker_init_fn=data.worker_init_fn)
+
+# For visualizations
+vis_loader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=1, shuffle=False,
+    collate_fn=data.collate_remove_none,
+    worker_init_fn=data.worker_init_fn)
+model_counter = defaultdict(int)
+data_vis_list = []
+
+# Build a data dictionary for visualization
+iterator = iter(vis_loader)
+for i in range(len(vis_loader)):
+    data_vis = next(iterator)
+    idx = data_vis['idx'].item()
+    model_dict = val_dataset.get_model_dict(idx)
+    category_id = model_dict.get('category', 'n/a')
+    category_name = val_dataset.metadata[category_id].get('name', 'n/a')
+    category_name = category_name.split(',')[0]
+    if category_name == 'n/a':
+        category_name = category_id
+
+    c_it = model_counter[category_id]
+    if c_it < vis_n_outputs:
+        data_vis_list.append({'category': category_name, 'it': c_it, 'data': data_vis})
+
+    model_counter[category_id] += 1
+
+# Model
+model = config.get_model(cfg, device=device, dataset=train_dataset)
+
+# Generator
+generator = config.get_generator(model, cfg, device=device)
+
+# Intialize training
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+# optimizer = optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
+trainer = config.get_trainer(model, optimizer, cfg, device=device)
+
+checkpoint_io = CheckpointIO(out_dir, model=model, optimizer=optimizer)
+try:
+    load_dict = checkpoint_io.load('model.pt')
+except FileExistsError:
+    load_dict = dict()
+epoch_it = load_dict.get('epoch_it', 0)
+it = load_dict.get('it', 0)
+it_best = load_dict.get('it_best', 0)
+metric_val_best = load_dict.get(
+    'loss_val_best', -model_selection_sign * np.inf)
+
+if metric_val_best == np.inf or metric_val_best == -np.inf:
+    metric_val_best = -model_selection_sign * np.inf
+print('Current best validation metric (%s): %.8f'
+      % (model_selection_metric, metric_val_best))
+logger = SummaryWriter(os.path.join(out_dir, 'logs'))
+
+# Shorthands
+print_every = cfg['training']['print_every']
+checkpoint_every = cfg['training']['checkpoint_every']
+validate_every = cfg['training']['validate_every']
+visualize_every = cfg['training']['visualize_every']
+max_epoch = cfg['training']['epochs']
+lr_sched = cfg['training']['lr_sched']
+scheduled_rates = [
+    (0, 1e-4),
+    (100, 3e-5),
+    (200, 1e-5),
+    (300, 3e-6),
+    (400, 1e-6)
+]
+
+# Print model
+nparameters = sum(p.numel() for p in model.parameters())
+print('Total number of parameters: %d' % nparameters)
+
+print('output path: ', cfg['training']['out_dir'])
+
+# WANDB init
+if hasWandB:
+    wandb.init(project='gag-refine', config=cfg)
+
+while epoch_it < max_epoch:
+    epoch_it += 1
+
+    if lr_sched and scheduled_rates:
+        if epoch_it > scheduled_rates[0][0]:
+            ep, new_lr = scheduled_rates.pop(0)
+            print(f'updating learning rate after epoch {ep} to new LR {new_lr}')
+            trainer.optimizer = optim.Adam(model.parameters(), lr=new_lr)
+
+    for batch in train_loader:
+        it += 1
+        loss = trainer.train_step(batch)
+        if not isinstance(loss, dict):
+            loss = {
+                'loss': loss
+            }
+        log_dict = {
+            'train/iteration': it,
+            'train/epoch': epoch_it
+        }
+        for key, val in loss.items():
+            log_dict[f'train/{key}'] = val
+            logger.add_scalar(f'train/{key}', val, it)
+
+        if hasWandB:
+            wandb.log(log_dict)
+
+        # Print output
+        if print_every > 0 and (it % print_every) == 0:
+            t = datetime.datetime.now()
+            elapsed_time_str = str(datetime.timedelta(seconds=time.time() - t0)).split('.')[0]  # remove milliseconds
+            loss_str = ''
+            for key, val in loss.items():
+                loss_str += f'{key}: {val:.4f}, '
+            loss_str = loss_str[:-2]
+            print(f'[{t.hour:02d}:{t.minute:02d}]-[{elapsed_time_str}] ep: {epoch_it:03d}, it: {it:06d}, {loss_str}')
+
+    # Visualize output
+    if visualize_every > 0 and (epoch_it % visualize_every) == 0:
+        print('Visualizing')
+        for data_vis in data_vis_list:
+            if cfg['generation']['sliding_window']:
+                out = generator.generate_mesh_sliding(data_vis['data'])
+            else:
+                out = generator.generate_mesh(data_vis['data'])
+            # Get statistics
+            try:
+                mesh, stats_dict = out
+            except TypeError:
+                mesh, stats_dict = out, {}
+
+            mesh.export(os.path.join(out_dir, 'vis', '{}_{}_{}.off'.format(it, data_vis['category'], data_vis['it'])))
+
+    # Save checkpoint
+    if (checkpoint_every > 0 and (epoch_it % checkpoint_every) == 0):
+        print('Saving checkpoint')
+        checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                           loss_val_best=metric_val_best, it_best=it_best)
+
+    # Backup if necessary
+    if (backup_every > 0 and (epoch_it % backup_every) == 0):
+        print('Backup checkpoint')
+        checkpoint_io.save(f'model_{epoch_it:d}.pt', epoch_it=epoch_it, it=it,
+                           loss_val_best=metric_val_best, it_best=it_best)
+    # Run validation
+    if validate_every > 0 and (epoch_it % validate_every) == 0:
+        eval_dict = trainer.evaluate(val_loader)
+
+        metric_val = eval_dict[model_selection_metric]
+        print(f'validation metrics ({model_selection_metric} used to determine model_best.pt)')
+        for key, val in eval_dict.items():
+            print(f'\t{key:17s} {val:.5f}')
+        print(f'last best model was at it {it_best} with val score {metric_val_best}')
+
+        log_dict = {'val/epoch': epoch_it, 'val/iteration': it}
+        for k, v in eval_dict.items():
+            logger.add_scalar('val/%s' % k, v, it)
+            log_dict[f'val/{k}'] = v
+
+        if hasWandB:
+            wandb.log(log_dict)
+
+        if model_selection_sign * (metric_val - metric_val_best) > 0:
+            metric_val_best = metric_val
+            it_best = it
+            print(f'found new best model at it {it} with val score {metric_val_best:.4f}')
+            checkpoint_io.save('model_best.pt', epoch_it=epoch_it, it=it,
+                               loss_val_best=metric_val_best, it_best=it_best)
+
+    # Exit if necessary
+    if exit_after > 0 and (time.time() - t0) >= exit_after:
+        print('Time limit reached. Exiting.')
+        break
+
+print('finished. saving model checkpoint.')
+checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                   loss_val_best=metric_val_best, it_best=it_best)
+print('bye bye.')
---
+++ tests/grasp_testing.py
@@ -0,0 +1,311 @@
+import os
+from timeit import default_timer as timer
+import numpy as np
+import configparser
+import yaml
+import open3d as o3d
+
+import burg_toolkit as burg
+
+SAVE_FILE = os.path.join('..', 'sampled_grasps.npy')
+
+
+def test_distance_and_coverage():
+    # testing the distance function
+    initial_translations = np.random.random((50, 3))
+    gs = burg.grasp.GraspSet.from_translations(initial_translations)
+
+    theta = 0 / 180 * np.pi
+    rot_mat = np.asarray([[1, 0, 0],
+                          [0, np.cos(theta), -np.sin(theta)],
+                          [0, np.sin(theta), np.cos(theta)]])
+
+    grasp = gs[0]
+    grasp.translation = np.asarray([0, 0, 0.003])
+    grasp.rotation_matrix = rot_mat
+    gs[0] = grasp
+    print(grasp)
+
+    theta = 15 / 180 * np.pi
+    rot_mat = np.asarray([[np.cos(theta), 0, np.sin(theta)],
+                          [0, 1, 0],
+                          [-np.sin(theta), 0, np.cos(theta)]])
+
+    grasp = gs[1]
+    grasp.translation = np.asarray([0, 0, 0])
+    grasp.rotation_matrix = rot_mat
+    gs[1] = grasp
+    print(grasp)
+
+    print('average gripper point distances between 20 and 50 elem graspset')
+    print(burg.metrics.avg_gripper_point_distances(gs[0:20], gs).shape)
+
+    dist = burg.metrics.combined_distances(gs[0], gs[1])
+    print('computation of pairwise_distances (15 degree and 3 mm)', dist.shape, dist)
+
+    t1 = timer()
+    print('computation of coverage 20/50:', burg.metrics.coverage_brute_force(gs, gs[0:20]))
+    print('this took:', timer() - t1, 'seconds')
+
+    t1 = timer()
+    print('coverage kd-tree:', burg.metrics.coverage(gs, gs[0:20], print_timings=True))
+    print('this took:', timer() - t1, 'seconds')
+
+    grasp_folder = 'e:/datasets/21_ycb_object_grasps/'
+    grasp_file = '061_foam_brick/grasps.h5'
+    grasp_set, com = burg.io.read_grasp_file_eppner2019(os.path.join(grasp_folder, grasp_file))
+
+    t1 = timer()
+    # this is unable to allocate enough memory for len(gs)=500
+    #print('computation of coverage 20/50:', gdt.grasp.coverage_brute_force(grasp_set, gs))
+    #print('this took:', timer() - t1, 'seconds')
+
+    t1 = timer()
+    print('coverage kd-tree:', burg.metrics.coverage(grasp_set, gs, print_timings=True))
+    print('in total, this took:', timer() - t1, 'seconds')
+
+
+
+def test_new_antipodal_grasp_sampling():
+    redo = []
+    gripper_model = burg.gripper.ParallelJawGripper(finger_length=0.03,
+                                                    finger_thickness=0.003)
+    with open("objects.txt", "r") as file:
+        object_names = file.read().splitlines()
+
+    for object_name in object_names:
+        mesh_fn = f"C:/Users/anizy/OneDrive - Aston University/Desktop/Dissertation/pointcloud/models/ycb/{object_name}/google_16k/textured.obj"
+
+        #mesh_fn= '../input/002_master_chef_can_pointcloud.ply'
+        #obj_name = os.path.splitext(os.path.basename(mesh_fn))[0]
+        #mesh = o3d.io.read_triangle_mesh(mesh_fn)
+        #n_sample = np.max([500, 1000, len(mesh.triangles)])
+        #pc_with_normals = burg.util.o3d_pc_to_numpy(burg.mesh_processing.poisson_disk_sampling(mesh, n_points=n_sample))
+        #pc_with_normals = burg.util.o3d_pc_to_numpy(burg.mesh_processing.poisson_disk_sampling(mesh))
+        #np.save(object_name + '_pc.npy', pc_with_normals)
+
+        ags = burg.sampling.AntipodalGraspSampler()
+        ags.mesh = burg.io.load_mesh(mesh_fn)
+        ags.gripper = gripper_model
+        ags.n_orientations = 1
+        ags.verbose = True
+        ags.max_targets_per_ref_point = 1
+        graspset, contacts, pc_with_normals = ags.sample(500)
+        np.save(object_name + '_pc.npy', pc_with_normals)
+        #print(contacts[0])
+        #contacts_with_mscore = np.copy(contacts)
+        # for c in contacts:
+        #     nc = np.concatenate((c[:2], np.expand_dims(c[2], axis=0)))
+        #     contacts_with_mscore.append(nc)
+        # contacts_with_mscore = np.array(contacts_with_mscore)
+        #contacts_with_mscore[:, 2:5] = contacts_with_mscore[:, 2:3]
+        # contacts_with_mscore = np.array([list(np.unique(row)) for row in contacts])
+        # print(contacts_with_mscore[0])
+        graspset.scores = ags.check_collisions(graspset, use_width=False)  # need to install python-fcl
+        filtered_grasps_contacts = {}
+        filtered_grasps_contacts['points'] = []
+        filtered_grasps_contacts['normals'] = []
+        filtered_grasps_contacts['angles'] = []
+        filtered_grasps_contacts['score'] = []
+        for grasp, score, contact,normals,angles,cp_score in zip(graspset, graspset.scores, contacts['points'],contacts['normals'],contacts['angles'],contacts['score']):
+            if score == 1.0:
+                filtered_grasps_contacts['points'].append(contact)
+                filtered_grasps_contacts['normals'].append(normals)
+                filtered_grasps_contacts['angles'].append(angles)
+                filtered_grasps_contacts['score'].append(cp_score)
+
+        if len(filtered_grasps_contacts['points']) < 90:
+            redo.append(object_name)
+        print(redo)
+        np.savez(object_name + "_test.npz", **filtered_grasps_contacts)
+
+def test_antipodal_grasp_random_sampling():
+    redo = []
+    gripper_model = burg.gripper.ParallelJawGripper(finger_length=0.03,
+                                                    finger_thickness=0.003)
+    with open("objects.txt", "r") as file:
+        object_names = file.read().splitlines()
+
+    for object_name in object_names:
+        mesh_fn = f"C:/Users/anizy/OneDrive - Aston University/Desktop/Dissertation/pointcloud/models/ycb/{object_name}/google_16k/textured.obj"
+        ags = burg.sampling.AntipodalGraspSampler()
+        ags.mesh = burg.io.load_mesh(mesh_fn)
+        ags.gripper = gripper_model
+        ags.n_orientations = 1
+        ags.verbose = True
+        ags.max_targets_per_ref_point = 1
+        graspset, contacts = ags.randomsample(5000)
+        graspset.scores = ags.check_collisions(graspset, use_width=False)
+        filtered_grasps_contacts = {}
+        filtered_grasps_contacts['points'] = []
+        filtered_grasps_contacts['normals'] = []
+        filtered_grasps_contacts['angles'] = []
+        filtered_grasps_contacts['score'] = []
+        for grasp, score, contact,normals,angles,cp_score in zip(graspset, graspset.scores, contacts['points'],contacts['normals'],contacts['angles'],contacts['score']):
+            if score == 1.0:
+                filtered_grasps_contacts['points'].append(contact)
+                filtered_grasps_contacts['normals'].append(normals)
+                filtered_grasps_contacts['angles'].append(angles)
+                filtered_grasps_contacts['score'].append(cp_score)
+
+        if len(filtered_grasps_contacts['points']) < 1500:
+            redo.append(object_name)
+        print(redo)
+        np.savez(object_name + "_random.npz", **filtered_grasps_contacts)
+
+
+
+def test_rotation_to_align_vectors():
+    vec_a = np.array([1, 0, 0])
+    vec_b = np.array([0, 1, 0])
+    r = burg.util.rotation_to_align_vectors(vec_a, vec_b)
+    print('vec_a', vec_a)
+    print('vec_b', vec_b)
+    print('R*vec_a', np.dot(r, vec_a.reshape(3, 1)))
+
+    vec_a = np.array([1, 0, 0])
+    vec_b = np.array([-1, 0, 0])
+    r = burg.util.rotation_to_align_vectors(vec_a, vec_b)
+    print('vec_a', vec_a)
+    print('vec_b', vec_b)
+    print('R*vec_a', np.dot(r, vec_a.reshape(3, 1)))
+
+
+def show_grasp_pose_definition():
+    gs = burg.grasp.GraspSet.from_translations(np.asarray([0, 0, 0]).reshape(-1, 3))
+    gripper = burg.gripper.ParallelJawGripper(finger_length=0.03, finger_thickness=0.003, opening_width=0.05)
+    burg.visualization.show_grasp_set([o3d.geometry.TriangleMesh.create_coordinate_frame(0.02)],
+                                      gs, gripper=gripper)
+
+
+def test_angles():
+    vec_a = np.array([-0.037533,  0.00233,  -0.041748])
+    vec_b = np.array([0.6694,  -0.04297, 0.7417 ])
+    mask = np.array([0])
+    print(mask)
+    a = burg.util.angle(vec_a, vec_b, sign_array=mask, as_degree=False)
+    print(a)
+    print(mask)
+
+
+def test_cone_sampling():
+    axis = [0, 1, 0]
+    angle = np.pi/4
+    rays = burg.sampling.rays_within_cone(axis, angle, n=100)
+
+    print(rays.shape)
+
+
+def visualise_perturbations():
+    positions = np.zeros((3, 3))
+    positions[0] = [0.3, 0, 0]
+    positions[1] = [0, 0, 0.3]
+
+    gs = burg.grasp.GraspSet.from_translations(positions)
+    gs_perturbed = burg.sampling.grasp_perturbations(gs, radii=[5, 10, 15])
+    gripper = burg.gripper.ParallelJawGripper(finger_length=0.03, finger_thickness=0.003, opening_width=0.05)
+    burg.visualization.show_grasp_set([], gs_perturbed, gripper=gripper)
+
+def test_scoring_for_grippers():
+    #how-to convert this mesh to the scene data  in the visualisation
+    #point cloud from scenes
+    data = np.load("C:/Users/anizy/OneDrive - Aston University/Documents/GraspQualityEstimator/scenes/002_master_chef_can/full_point_cloud.npz")
+    # data = np.load(
+    #     "C:/Users/anizy/OneDrive - Aston University/Documents/fork/burg-toolkit/tests/dataset/002_master_chef_can.npz")
+    points = data['points']
+    point_cloud = o3d.geometry.PointCloud()
+    point_cloud.points = o3d.utility.Vector3dVector(points)
+
+    #mesh_fn = "C:/Users/anizy/OneDrive - Aston University/Documents/GraspQualityEstimator/scenes/002_master_chef_can/pointcloud.ply"
+    #mesh_fn = "C:/Users/anizy/OneDrive - Aston University/Desktop/Dissertation/pointcloud/models/ycb/002_master_chef_can/google_16k/textured.obj"
+    #mesh = burg.io.load_mesh(mesh_fn)
+    # cp1 = np.array([-0.06323,   -0.02559,    0.000794])
+    # cp2 = np.array([0.0005007, -0.0356,     0.003418])
+    # normal1 = np.array([0.8794,  0.3086, -0.3623])
+    # normal2 = np.array([-0.1969,  0.2625, -0.945])
+    cp1 = np.array([0.12141492, 0.11315102, 0.21749111])
+    cp2 = np.array([0.13996961, 0.19805092, 0.10788723])
+    normal1 = np.array([-0.12878418, -0.21923828,  0.96728516])
+    normal2 = np.array([-0.20227051,  0.97949219, -0.00253677])
+    d = (cp2-cp1)
+
+    angle_cp1, angle_cp2, score = burg.util.calc_score(d,normal1, normal2)
+    print(angle_cp1)
+    print(angle_cp2)
+    print('score:' + str(score))
+    burg.visualization.plot_contacts_normals(point_cloud,cp1,cp2,normal1,normal2, score)
+
+
+def transform_point(point, transformation_matrix):
+    homogeneous_point = np.append(point, 1)
+    #append 0 for normals
+    transformed_homogeneous_point = np.dot(transformation_matrix, homogeneous_point)
+    transformed_point = transformed_homogeneous_point[:3] / transformed_homogeneous_point[3]
+    #vectors verify this above step
+    return transformed_point
+
+def transform_normal(normal, transformation_matrix):
+    homogeneous_normal = np.append(normal, 0)  # Append 0 for the normal
+    transformed_homogeneous_normal = np.dot(transformation_matrix, homogeneous_normal)
+    transformed_normal = transformed_homogeneous_normal[:3] / 1
+    return transformed_normal
+
+def augmented_tranformation():
+    folder_path = "C:/Users/anizy/OneDrive - Aston University/Documents/GraspQualityEstimator/tests/dataset/"
+    yaml_fol = "C:/Users/anizy/OneDrive - Aston University/Documents/GraspQualityEstimator/scenes/"
+    output_folder = "C:/Users/anizy/OneDrive - Aston University/Documents/GraspQualityEstimator/transformed_data/"
+
+    if os.path.exists(folder_path):
+        file_list = os.listdir(folder_path)
+        for file_name in file_list:
+            yaml_fol_path = os.path.join(yaml_fol, file_name.replace('.npz', '/'))
+            if os.path.isdir(yaml_fol_path):
+                yaml_file_path = os.path.join(yaml_fol_path, "scene.yaml")
+                with open(yaml_file_path, "r") as yaml_file:
+                    scene_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+                    if 'objects' in scene_data and len(scene_data['objects']) > 0 and 'pose' in scene_data['objects'][0]:
+                        pose_data = scene_data['objects'][0]['pose']
+                        transformation_matrix = np.array(pose_data)
+                        file_path = os.path.join(folder_path, file_name)
+                        data = np.load(file_path)
+                        updated_data = {}
+                        for key in data.keys():
+                            if key == 'points':
+                                points_array = data['points']
+                                transformed_points_array = []
+
+                                for point_pair in points_array:
+                                    CP1 = point_pair[0]
+                                    CP2 = point_pair[1]
+                                    transformed_CP1 = transform_point(CP1, transformation_matrix)
+                                    transformed_CP2 = transform_point(CP2, transformation_matrix)
+                                    transformed_points_array.append([transformed_CP1, transformed_CP2])
+                                updated_data['points'] = np.array(transformed_points_array)
+                            elif key == 'normals':
+                                normals_array = data['normals']
+                                transformed_normals_array = []
+                                for normal_pair in normals_array:
+                                    nr1 = normal_pair[0]
+                                    nr2 = normal_pair[1]
+                                    transformed_nr1 = transform_normal(nr1, transformation_matrix)
+                                    transformed_nr2 = transform_normal(nr2, transformation_matrix)
+                                    transformed_normals_array.append([transformed_nr1, transformed_nr2])
+                                updated_data['normals'] = np.array(transformed_normals_array)
+                            else:
+                                updated_data[key] = data[key]
+                        output_file_path = os.path.join(output_folder, file_name)
+                        np.savez(output_file_path, **updated_data)
+
+if __name__ == "__main__":
+    print('hi')
+    # test_distance_and_coverage()
+    # test_rotation_to_align_vectors()
+    #test_angles()
+    # test_cone_sampling()
+    # test_new_antipodal_grasp_sampling()
+    #show_grasp_pose_definition()
+    #visualise_perturbations()
+    test_scoring_for_grippers()
+    #test_antipodal_grasp_random_sampling()
+    #augmented_tranformation()
+    print('bye')
